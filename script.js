c2b6c66e8e070f5aa5a5d55b41af7d34225cb9d6
farfouze <88578003+farfouze@users.noreply.github.com>
2024-03-01 17:45:23 +0100

    Update script.js
    loader fix after 5s

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 window.onload = function() {
     var image = new Image();
+    var timeout = false;
+
     image.src = "https://starlightskins.lunareclipse.studio/skin-render/default/farfouze/bust";
     image.onload = function() {
+        if (!timeout) {
+            var loader = document.getElementById("loader");
+            loader.style.opacity = 0;
+            setTimeout(function() {
+                loader.style.display = "none";
+            }, 500);
+
+            var content = document.getElementById("content");
+            content.style.opacity = 0;
+            content.style.display = "block";
+            setTimeout(function() {
+                content.style.opacity = 1;
+            }, 500);
+        }
+    };
+
+    setTimeout(function() {
+        timeout = true;
         var loader = document.getElementById("loader");
         loader.style.opacity = 0;
         setTimeout(function() {
@@ -9,10 +29,12 @@ window.onload = function() {
         }, 500);
 
         var content = document.getElementById("content");
-        content.style.opacity = 0;
+        content.style.opacity = 1;
         content.style.display = "block";
-        setTimeout(function() {
-            content.style.opacity = 1;
-        }, 500);
+    }, 5000);
+
+    image.onerror = function() {
+        timeout = true;
+        image.src = "";
     };
 };
